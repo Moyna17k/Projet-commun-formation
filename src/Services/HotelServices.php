e84495117ad017b9183c6c1526e9d69ecc21fef6
pierreb <p-billot@outlook.fr>
2024-06-04 15:21:26 +0200

    un message

--- src/Services/HotelServices.php
+++ src/Services/HotelServices.php
@@ -6,39 +6,38 @@ class HotelServices
 {
     public function hotel()
     {
-       
+
         // URL to send the request to
         $url = 'https://recherche-entreprises.api.gouv.fr/search?activite_principale=55.10Z&code_postal=17000';
-        
+
         // Initialize a cURL session
         $ch = curl_init();
-        
+
         // Set the URL
         curl_setopt($ch, CURLOPT_URL, $url);
-        
+
         // Set the option to return the result as a string
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        
+
         // Execute the cURL session and store the result
         $response = curl_exec($ch);
 
         // json decode 
-
         $response = json_decode($response, true);
 
-        // je veux récupéré que les nom des entreprise
+        // je veux récupérer tous les noms des entreprises
         $r = $response['results'];
 
-        foreach($r as $value ) {
+        $nomCompletArray = [];
 
-            dd($value['nom_complet']);
+        foreach ($r as $value) {
+            $nomCompletArray[] = $value['nom_complet'];
         }
-        
+
         // Close the cURL session
         curl_close($ch);
- 
-        
-        
 
+        // Return the array of nom_complet values
+        return $nomCompletArray;
     }
 }
