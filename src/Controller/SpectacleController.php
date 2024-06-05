5743df20c9d6ad9b45a1c3b322a5de34b03a74bb
Bedrech <jr17000@live.fr>
2024-06-05 10:23:10 +0200

    api

--- src/Controller/SpectacleController.php
+++ src/Controller/SpectacleController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Services\SpectacleServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SpectacleController extends AbstractController
 {
+    public function __construct(
+        Private SpectacleServices $spectacle
+    ){}
     #[Route('/spectacle', name: 'app_spectacle')]
     public function index(): Response
     {
         return $this->render('spectacle/index.html.twig', [
-            'controller_name' => 'SpectacleController',
+            'spectacle' => $this->spectacle->spectacle(),
         ]);
     }
 }
