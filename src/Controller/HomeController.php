637104f8532a94b2a2c0fac389e17c4fc05d6f68
Sacha <sacha.hemon@gmail.com>
2024-06-06 21:53:48 +0200

    Modifications du homeController pour recuperer le data de l'api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Services\HotelServices;
+use App\Services\RestaurantServices;
+use App\Services\SpectacleServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        Private SpectacleServices $spectacle,
+        Private RestaurantServices $restaurant,
+        Private HotelServices $hotel
+    ){}
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'spectacle' => $this->spectacle->spectacle(),
+            'restaurant' => $this->restaurant->restaurant(),
+            'hotel' => $this->hotel->hotel(),
         ]);
     }
 }
