cd075874fed07e9a9802f9189a00dc58bbfde2be
Moyna17k <nyo54000@gmail.com>
2024-06-06 15:22:50 +0200

    chore: Update mailer configuration and remove conflicting code

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,11 +46,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-<<<<<<< HEAD
                     ->from(new Address('localhost:1025', 'ACGD Tourisme'))
-=======
-                    ->from(new Address('toto@gmail.com', 'ACGD Tourisme'))
->>>>>>> 678f33163072cacb169660f6206a00ce28d528e9
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
